d8d202bac3a20cb3921baaeb168f62f3d42b9e17
Endre Nemeth <endre.nemeth@nokia.com>
2019-10-15 16:21:57 +0200

    robot tcs, test charts, robot container added
    Change-Id: I62fc3e2921f1dbbee5338bf22e15d322b5d49ccc
    Signed-off-by: Endre Nemeth <endre.nemeth@nokia.com>

---
+++ resources/test_containers/http-traffic-gen/http_traffic_gen.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import thread
+import time
+import traceback
+import requests
+
+
+URL = "http://podinfo.kube-system.svc.rec.io:9898"
+LOCATION = "nokia"
+PARAMS = {'address': LOCATION}
+DELTA = 0.01
+
+
+def worker():
+    for _ in range(0, 1000):
+        time.sleep(DELTA)
+        requests.get(url=URL, params=PARAMS)
+
+try:
+    for x in range(0, 3):
+        thread.start_new_thread(worker, ("Thread-" + str(x), ))
+except Exception as e:
+    traceback.print_exc()
+
+time.sleep(40)
